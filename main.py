95b1a82d0a3c4311346fc709b9b74eda50a85e5b
aaron-cherry <aaron.cherry10@gmail.com>
2025-07-05 22:57:18 -0700

    adding fps cap

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"{pygame.get_init()}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
